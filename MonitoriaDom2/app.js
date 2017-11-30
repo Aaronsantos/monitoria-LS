339c63ab7ebf6269da1c406a94ea97e69dcfa8ac
aaronsantos <aaronpedro16@gmail.com>
2017-11-30 12:13:17 -0300

    Adicionando Exercício 2 de DOM

---
+++ MonitoriaDom2/app.js
@@ -0,0 +1,13 @@
+//O que já sabemos:
+// let tag = document.querySelector('p') ---> Seleciona a primeira tag P
+// tag.innerHTML = "Hell World!" ---> Insere "Hello World" no conteudo da tag P selecionada
+// tag.addEventListener("click", function().....) ---> Adciona um listener, que quando acionada, chamará a dada função
+
+//Oq veremos:
+//event.target
+//elem.remove()
+//elem.parentNode
+//elem.focus()
+
+//todo:
+//implementar o cadastro de novos membros. (sem se preocupar com o botão "excomungar")
